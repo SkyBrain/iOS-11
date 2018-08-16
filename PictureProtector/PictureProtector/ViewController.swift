43a6c8c505183670b05b59c30c1d2dc7f0938bfd
SampathKumar <sampath53@hotmail.com>
2018-08-16 17:50:08 -0400

    Final Project

--- PictureProtector/PictureProtector/ViewController.swift
+++ PictureProtector/PictureProtector/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -14,9 +15,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     var inputImage:UIImage?
     
+    var detectedFaces = [(observation:VNFaceObservation, blur:Bool)]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Import", style: .plain, target: self, action: #selector(importPhoto))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(sharePhoto))
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,7 +42,106 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         inputImage = image
         
         dismiss(animated: true) {
-            
+            self.detectFaces()
         }
+    }
+    
+    func detectFaces(){
+        guard let inputImage = inputImage else { return }
+        guard let ciImage = CIImage(image: inputImage) else { return }
+        
+        let request = VNDetectFaceRectanglesRequest { [weak self] request,error in
+            if let error = error{
+                print(error.localizedDescription)
+            }
+            else{
+                guard let observations = request.results as? [VNFaceObservation] else {return}
+                self?.detectedFaces = Array(zip(observations, [Bool](repeating: false, count: observations.count)))
+                self?.addBlurRects()
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func addBlurRects(){
+        imageView.subviews.forEach{ $0.removeFromSuperview()}
+        let imageRect = imageView.contentClippingRect
+        
+        for (index,face) in detectedFaces.enumerated(){
+            let boundingBox = face.observation.boundingBox
+            let size = CGSize(width: boundingBox.width * imageRect.width, height: boundingBox.height * imageRect.height)
+            var origin = CGPoint(x: boundingBox.minX * imageRect.width, y: (1 - face.observation.boundingBox.minY) * imageRect.height - size.height)
+            origin.y += imageRect.minY
+            let vw = UIView(frame: CGRect(origin: origin, size: size))
+            vw.tag = index
+            vw.layer.borderColor = UIColor.red.cgColor
+            vw.layer.borderWidth = 2
+            imageView.addSubview(vw)
+            
+            let recognizer = UITapGestureRecognizer(target: self, action: #selector(faceTapped))
+            vw.addGestureRecognizer(recognizer)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        addBlurRects()
+    }
+    
+    func renderBlurredFaces(){
+        guard let currentUIImage = inputImage else { return }
+        guard let currentCGImage = currentUIImage.cgImage else { return }
+        let currentCIImage = CIImage(cgImage: currentCGImage)
+        
+        let filter = CIFilter(name: "CIPixellate")
+        filter?.setValue(currentCIImage, forKey: kCIInputImageKey)
+        filter?.setValue(12, forKey: kCIInputScaleKey)
+        
+        guard let outputImage = filter?.outputImage  else { return }
+        let blurredImage = UIImage(ciImage: outputImage)
+        
+        let renderer = UIGraphicsImageRenderer(size: currentUIImage.size)
+        
+        let result = renderer.image { ctx in
+            currentUIImage.draw(at: .zero)
+            let path = UIBezierPath()
+            
+            for face in detectedFaces{
+                if face.blur{
+                    let boundingBox = face.observation.boundingBox
+                    let size = CGSize(width: boundingBox.width * currentUIImage.size.width, height: boundingBox.height * currentUIImage.size.height)
+                    var origin = CGPoint(x: boundingBox.minX * currentUIImage.size.width, y: (1 - face.observation.boundingBox.minY) * currentUIImage.size.height - size.height)
+                    
+                    let rect = CGRect(origin: origin, size: size)
+                    
+                    let miniPath = UIBezierPath(rect: rect)
+                    path.append(miniPath)
+                }
+            }
+            
+            if !path.isEmpty{
+                path.addClip()
+                blurredImage.draw(at: .zero)
+            }
+        }
+        imageView.image = result
+    }
+    
+    @objc func faceTapped(_ sender:UITapGestureRecognizer){
+        guard let vw = sender.view else { return }
+        detectedFaces[vw.tag].blur = !detectedFaces[vw.tag].blur
+        renderBlurredFaces()
+    }
+    
+    @objc func sharePhoto(){
+        guard let img = imageView.image else { return }
+        let ac = UIActivityViewController(activityItems: [img], applicationActivities: nil)
+        present(ac,animated: true)
     }
 }
